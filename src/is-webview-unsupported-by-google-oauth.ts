2efdee766eaa50246bd27a37887caa6797c78b37
Loïc Poullain <loic.poullain@protonmail.com>
2024-10-08 14:05:21 +0200

    Improve doc

--- src/is-webview-unsupported-by-google-oauth.ts
+++ src/is-webview-unsupported-by-google-oauth.ts
@@ -1,5 +1,5 @@
 /**
- * Check if the user agent is a user agent of a web view not supported by Google OAuth.
+ * Check if the user agent is a user agent of a web view that is not supported by Google OAuth.
  */
 export function isWebviewUnsupportedByGoogleOAuth(userAgent: string): boolean {
   return (
