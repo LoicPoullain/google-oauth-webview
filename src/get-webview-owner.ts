5c8448c4e0d9d4b5ded1aa190154a20050076755
Loïc Poullain <loic.poullain@protonmail.com>
2024-10-08 12:24:56 +0200

    Add doc

--- src/get-webview-owner.ts
+++ src/get-webview-owner.ts
@@ -1,5 +1,10 @@
 import { WebviewOwner } from "./interfaces";
 
+/**
+ * When possible, determine the owner of the webview based on the user agent.
+ *
+ * If the user agent does not match a webview or the webview owner cannot be determined, return null.
+ */
 export function getWebviewOwner(userAgent: string): WebviewOwner {
   throw new Error("Not implemented");
 }
--- src/is-webview-unsupported-by-google-oauth.ts
+++ src/is-webview-unsupported-by-google-oauth.ts
@@ -1,3 +1,6 @@
+/**
+ * Check if the user agent is a user agent of a web view not supported by Google OAuth.
+ */
 export function isWebviewUnsupportedByGoogleOAuth(userAgent: string): boolean {
   return (
     !userAgent.includes("LinkedInApp") && !userAgent.includes("FBAN/FBIOS")
