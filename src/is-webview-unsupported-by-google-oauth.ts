b45847965a9131388d8610a4b81dca0fa7e4cb66
richardx95 <richardcoopmans@hotmail.nl>
2024-12-11 10:27:54 +0100

    updated is-webview-unsupported-by-google-oauth

--- src/is-webview-unsupported-by-google-oauth.ts
+++ src/is-webview-unsupported-by-google-oauth.ts
@@ -2,5 +2,5 @@
  * Check if the user agent is a user agent of a web view that is not supported by Google OAuth.
  */
 export function isWebviewUnsupportedByGoogleOAuth(userAgent: string): boolean {
-  return userAgent.includes("LinkedInApp") || userAgent.includes("FBAN/FBIOS");
+  return userAgent.includes("LinkedInApp") || userAgent.includes("FBAN/FBIOS") || userAgent.includes("Instagram");
 }
